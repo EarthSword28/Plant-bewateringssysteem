6d55554a961124382e7dede0785fab55aadae7b9
EarthSword28 <jorden.van.nunen@gmail.com>
2025-02-17 11:22:44 +0100

    bodemvochtigheidssensor

--- Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
+++ Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
@@ -3,6 +3,15 @@
 
 #include <calibrate_bodemvochtigheidssensoren.h>
 
+#define RESISTANCE_HUMIDITY_SENSOR 36
+#define CAPACITIVE_HUMIDITY_SENSOR 39
+
+unsigned long resistanceHumidityValue = 0;
+unsigned long capacitanceHumidityValue = 0;
+
+const int TIME_INTERVAL = 2500;
+unsigned long timer1 = 0;
+
 void setup() {}
 
 void loop() {}
