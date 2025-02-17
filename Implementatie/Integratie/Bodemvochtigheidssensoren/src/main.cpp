90e0ee207722c43ae1fc438ccb34822966792520
EarthSword28 <jorden.van.nunen@gmail.com>
2025-02-17 12:05:09 +0100

    bodemvochtigheidssensor resistance category

--- Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
+++ Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
@@ -9,12 +9,28 @@
 unsigned long resistanceHumidityValue = 0;
 unsigned long capacitanceHumidityValue = 0;
 
-byte resistanceHumidityCategory = 0;      // Legenda: NO Measurement = 0, DRY = 1, WET = 2, WATER = 3
-byte capacitanceHumidityCategory = 0;     // Legenda: NO Measurement = 0, DRY = 1, WET = 2, WATER = 3
-byte finalHumidityCategory = 0;           // Legenda: NO Measurement = 0, DRY = 1, WET = 2, WATER = 3
+byte resistanceHumidityCategory = 0;
+byte capacitanceHumidityCategory = 0;
+byte finalHumidityCategory = 0;
+const byte LEGENDA_NO_VALUE = 0;
+const byte LEGENDA_DRY = 1;
+const byte LEGENDA_WET = 2;
+const byte LEGENDA_WATER = 3;
 
 const int TIME_INTERVAL = 2500;
 unsigned long timer = 0;
+
+byte get_resistance_category(int sensorValue) {
+  if (sensorValue >= RESISTANCE_SENSOR_DRY_INTERVAL_MIN && sensorValue <= RESISTANCE_SENSOR_DRY_INTERVAL_MAX) {
+    return LEGENDA_DRY;
+  }
+  if (sensorValue >= RESISTANCE_SENSOR_WET_INTERVAL_MIN && sensorValue <= RESISTANCE_SENSOR_WET_INTERVAL_MAX) {
+    return LEGENDA_WET;
+  }
+  if (sensorValue >= RESISTANCE_SENSOR_WATER_INTERVAL_MIN && sensorValue <= RESISTANCE_SENSOR_WATER_INTERVAL_MAX) {
+    return LEGENDA_WATER;
+  }
+}
 
 void setup() {
   pinMode(RESISTANCE_HUMIDITY_SENSOR, INPUT);
@@ -29,6 +45,7 @@ void setup() {
 void loop() {
   if (millis() - timer >= TIME_INTERVAL) {
     timer = millis();
+
     resistanceHumidityValue = analogRead(RESISTANCE_HUMIDITY_SENSOR);
     capacitanceHumidityValue = analogRead(CAPACITANCE_HUMIDITY_SENSOR);
   }
