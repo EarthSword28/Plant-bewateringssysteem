858d8616d597c0985cc4793bceee674699d250c9
EarthSword28 <jorden.van.nunen@gmail.com>
2025-02-19 11:37:35 +0100

    constanten

--- Implementatie/Integratie/PlantBewateingssysteem/src/constants.h
+++ Implementatie/Integratie/PlantBewateingssysteem/src/constants.h
@@ -1,41 +1,47 @@
-// Minimum- en maximumvalue of the resistance humidity sensor to recieve the interpretation "DRY"
-const int RESISTANCE_SENSOR_DRY_INTERVAL_MIN = 0;
-const int RESISTANCE_SENSOR_DRY_INTERVAL_MAX = 2060;
-
-// Minimum- en maximumvalue of the resistance humidity sensor to recieve the interpretation "WET"
-const int RESISTANCE_SENSOR_WET_INTERVAL_MIN = 2061;
-const int RESISTANCE_SENSOR_WET_INTERVAL_MAX = 2546;
-
-// Minimum- en maximumvalue of the resistance humidity sensor to recieve the interpretation "WATER"
-const int RESISTANCE_SENSOR_WATER_INTERVAL_MIN = 2547;
-const int RESISTANCE_SENSOR_WATER_INTERVAL_MAX = 4095;
- 
-// Minimum- en maximumvalue of the capacitance humidity sensor to recieve the interpretation "DRY"
-const int CAPACITANCE_SENSOR_DRY_INTERVAL_MIN = 2872;
-const int CAPACITANCE_SENSOR_DRY_INTERVAL_MAX = 4095;
-
-// Minimum- en maximumvalue of the capacitance humidity sensor to recieve the interpretation "WET"
-const int CAPACITANCE_SENSOR_WET_INTERVAL_MIN = 2464;
-const int CAPACITANCE_SENSOR_WET_INTERVAL_MAX = 2871;
-
-// Minimum- en maximumvalue of the capacitance humidity sensor to recieve the interpretation "WATER"
-const int CAPACITANCE_SENSOR_WATER_INTERVAL_MIN = 0;
-const int CAPACITANCE_SENSOR_WATER_INTERVAL_MAX = 2463;
-
-// Legenda humidity
-const int HUMIDITY_NO_VALUE = 0;
-const int HUMIDITY_DRY = 1;
-const int HUMIDITY_WET = 2;
-const int HUMIDITY_WATER = 3;
-
-// Timers
+// DONE: Wachttijd tussen 2 opeenvolgende inlezingen van sensoren
 const int TIME_INTERVAL_SENSORS = 5000;
 
-  // watering timers
+// DONE: Temperatuur schakelwaarden
+const int MIN_TEMPERATURE = 5;
+const int MAX_TEMPERATURE = 25;
+
+// DONE: Categoriën vochtigheid
+const char HUMIDITY_NO_VALUE[] = "NO VALUE";
+const char HUMIDITY_DRY[] = "DRY";
+const char HUMIDITY_WET[] = "WET";
+const char HUMIDITY_WATER[] = "WATER";
+
+// DONE: Statussen water geven (geen water, wél water)
+const int WATER_OFF = 0;
+const int WATER_ON = 1;
+
+// DONE: Duurtijden water geven (in milliseconden)
 const int WATERING_TIME_INTERVAL_INACTIVE = 0;
 const int WATERING_TIME_INTERVAL_SHORT = 1000;
 const int WATERING_TIME_INTERVAL_LONG = 2000;
 
-// Temperature intervals
-const int MIN_TEMPERATURE = 5;
-const int MAX_TEMPERATURE = 25;
+// Intervallen voor BVH waarden
+// Opgelet, we definiëren de intervallen als gesloten: [min, max]
+// Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "DROOG" te krijgen
+const int RESISTIEVE_SENSOR_DROOG_INTERVAL_MIN = 0;
+const int RESISTIEVE_SENSOR_DROOG_INTERVAL_MAX = 2060;
+
+// Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "VOCHTIG" te krijgen
+const int RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MIN = 2061;
+const int RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MAX = 2546;
+
+// Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "NAT" te krijgen
+const int RESISTIEVE_SENSOR_NAT_INTERVAL_MIN = 2547;
+const int RESISTIEVE_SENSOR_NAT_INTERVAL_MAX = 4095;
+
+// Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "DROOG" te krijgen
+const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MIN = 2872;
+const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MAX = 4095;
+
+// Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "VOCHTIG" te krijgen
+const int CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MIN = 2464;
+const int CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MAX = 2871;
+
+// Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "NAT" te krijgen
+const int CAPACITIEVE_SENSOR_NAT_INTERVAL_MIN = 0;
+const int CAPACITIEVE_SENSOR_NAT_INTERVAL_MAX = 2463;
