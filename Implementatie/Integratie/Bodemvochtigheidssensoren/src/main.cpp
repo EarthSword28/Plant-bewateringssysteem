0c6d560e37a48da94b047d470122cc609710df43
EarthSword28 <jorden.van.nunen@gmail.com>
2025-02-19 10:22:06 +0100

    commentaar

--- Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
+++ Implementatie/Integratie/Bodemvochtigheidssensoren/src/main.cpp
@@ -4,12 +4,12 @@
 
 #include <constants.h>
 
-#define RESISTANCE_HUMIDITY_SENSOR 36     // resistieve vochtigheidssensor
-#define CAPACITANCE_HUMIDITY_SENSOR 39    // capacitieve vochtigheidssensor
+#define RESISTANCE_HUMIDITY_SENSOR 36
+#define CAPACITANCE_HUMIDITY_SENSOR 39
 
-#define ONE_WIRE_BUS 12                   // temperatuursensor
+#define ONE_WIRE_BUS 12                   // temperature sensor
 
-#define RELAY_MODULE 17                   // de relay voor de pomp
+#define RELAY_MODULE 17                   // the relay for the pump
 
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);    
