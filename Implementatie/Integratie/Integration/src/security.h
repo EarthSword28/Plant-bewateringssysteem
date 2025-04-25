611f7fb7740ffa6be8af6aebef3d037dbaf070e2
EarthSword28 <65962366+EarthSword28@users.noreply.github.com>
2025-04-25 14:46:15 +0200

    Delete Implementatie/Integratie/Integration directory

--- Implementatie/Integratie/Integration/src/security.h
+++
@@ -1,8 +0,0 @@
-const char* WIFI_SSID = "IoT";
-const char* WIFI_PASSWORD = "KdGIoT56!";
-
-const char* SSID = "vNM";
-const char* PASSWORD = "TPjoqu.1113";
-const char* GOOGLE_SCRIPT_DEPLOYMENT_ID  = "AKfycbzZ4TUpzgprXxoXoBTZOGcdzsyrFS-69OVCkl8yXRy058WIIsLLsjA1TodgvLFov5G1OA";
-
-const char* GRAFANA_API_KEY = "AIzaSyDImOedtAWt7o85pb6cbgLRos1fsLfi03Y";
